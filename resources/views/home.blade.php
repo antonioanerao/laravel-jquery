20b6de305317cd2048439d7a31843c6dd983c70d
Antonio Anerão <anerao.junior@gmail.com>
2019-12-04 11:45:02 -0500

    Adicionado link para página create

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,7 +14,7 @@
                         </div>
                     @endif
 
-                    You are logged in!
+                    <p>You are logged in! Go to <a href="{{ route('create') }}">create page</a></p>
                 </div>
             </div>
         </div>
